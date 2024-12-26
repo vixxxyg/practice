30e0cdd9f5adee30adcba1310cf9cd21ae738b8a
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-12-26 18:19:34 +0100

    14

--- app2.js
+++ app2.js
@@ -3,25 +3,24 @@
 //objeto literal - el objeto cuyas propiedades estan declaradas textualmente en el código
 
 const gato = {
-    nombre: 'Valiente',
-    duerme: 'true',
-    edad: '10',
-    enemigos: ["agua", "perros"],
+  nombre: "Valiente",
+  duerme: "true",
+  edad: "10",
+  enemigos: ["agua", "perros"],
 };
 
 console.log(gato);
 console.log(gato.enemigos[0]);
-console.log(gato['enemigos'][0]);
+console.log(gato["enemigos"][0]);
 
-
-// ----CRUD 
+// ----CRUD
 // CRUD es un acrónimo que se refiere a las cuatro operaciones básicas que se pueden realizar en una base de datos o sistema de gestión de información. Cada letra representa una operación específica:
 
 // C - Create (Crear):
 // Crear nuevos registros en la base de datos o sistema.
 // Ejemplo: Agregar un nuevo usuario o producto.
 
-gato.color = 'Azul';
+gato.color = "Azul";
 
 // R - Read (Leer):
 // Consultar o recuperar datos existentes.
@@ -48,35 +47,33 @@ console.log(gato);
 // enemigos: Array [ "agua", "perros" ]
 // nombre: "Valiente"
 
-
 // ----hasOwnProperty - .hasOwnProperty() devuelve true o false si se encuentra la propiedad o no
 // console.log(gato.hasOwnProperty('nombre')); -true
 // console.log(gato.hasOwnProperty('salud'));  -false
 
-if (gato.hasOwnProperty('nombre')) {
-    gato.nombre = 'Michi';
+if (gato.hasOwnProperty("nombre")) {
+  gato.nombre = "Michi";
 }
 
 console.log(gato.nombre);
 
-
 // ----- Objetos anidados
 
 const perro = {
-    nonbre: 'Luna',
-    duerme: 'true',
-    edad: 3,
-    enemigos: ['gatos'],
-    otros: {
-        amigos: ['Cobarde', 'Rex'],
-        favoritos: {
-            comida: {
-                fria: 'carne',
-                caliente: 'pollo',
-            }
-        }
-    }
-}
+  nonbre: "Luna",
+  duerme: "true",
+  edad: 3,
+  enemigos: ["gatos"],
+  otros: {
+    amigos: ["Cobarde", "Rex"],
+    favoritos: {
+      comida: {
+        fria: "carne",
+        caliente: "pollo",
+      },
+    },
+  },
+};
 
 console.log(perro.otros.amigos[0]);
 //console.log(perro.otros.favorito.comida); - error
@@ -87,33 +84,61 @@ console.clear();
 // -----el metodo
 
 const perro1 = {
-    nombre: 'Luna',
-    duerme: 'true',
-    edad: 3,
-    enemigos: ['gatos'],
-    comer: function() {
-        console.log('perro comiendo');
-    },
-    comerDos() {
-        console.log('perro comiendo dos');
-    },
-    comerTres(alimento) {
-        console.log("perro comiendo: " + alimento);
-    },
-    comerCuatro(alimento) {
-        console.log(`
+  nombre: "Luna",
+  duerme: "true",
+  edad: 3,
+  enemigos: ["gatos", "ratones"],
+  comer: function () {
+    console.log("perro comiendo");
+  },
+  comerDos() {
+    console.log("perro comiendo dos");
+  },
+  comerTres(alimento) {
+    console.log("perro comiendo: " + alimento);
+  },
+  comerCuatro(alimento) {
+    console.log(`
             ${this.nombre} esta comiendo ${alimento}
         `);
-    }
-}
+  },
+  comerCinco: (alimento) => {
+    console.log(`
+            ${this.nombre} esta comiendo ${alimento}
+            `);
+  },
+  comerCinco(alimento) {
+    console.log(`
+            ${this.nombre} esta comiendo ${alimento}
+            `);
+  },
+  listerEnemigos() {
+    this.enemigos.forEach((item) => console.log(item));
+  },
+};
 
 perro1.comer();
 perro1.comerDos();
 perro1.comerTres("pez");
 perro1.comerCuatro("carne");
+perro1.comerCinco("carne");
+perro1.listerEnemigos();
 
 // Console - perro comiendo
 // Console - perro comiendo dos
 // Console - perro comiendo: pez
 // Console - Luna esta comiendo carne
+
+//Arrow Function - No tiene this, o super y no se debe usarla como metodos
+
+//Console - undefined esta comiendo carne
+//Console - Luna esta comiendo carne
+
+//Pero dentro de un metodo - sí, podemos
+//Console - Gatos
+//          Ratones
+
 // this - hace referencia al objeto contexto de JavaScript en el cual se está ejecutando el código actual
+
+//Arrow Function - No tiene this, o super y no se debe usarla como metodos
+//Pero dentro de un metodo - sí, podemos
